15b06079741ea87f0cccaa493c833fe407e65c3c
JuanEsMorales <juanchito89768@gmail.com>
2023-08-09 17:00:40 -0500

    Haciendo los botones funcionales

--- games.js
+++ games.js
@@ -1,8 +1,10 @@
 let points = 50;
 let count = 3;
 let countPvt = 3; 
+let countDownPvt = 60;
 let seconds = 0;
 let tempF = setInterval(countFirst, 1000);
+
 function countFirst() {
   let txtCount;
   txtCount = count;
@@ -14,17 +16,40 @@ function countFirst() {
 }
 const greenButton = document.getElementById("green-button");
 const buttonPressed = document.getElementById("green-pressed");
-const min = 1;
-const max = 10;
-const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-console.log(randomNumber);
-greenButton.addEventListener('mousedown', ()=>{
+const clickButtons = document.querySelector(".click-buttons");
+const stopSign = document.getElementById("stop-sign");
+const greenButtons = document.querySelector(".green-buttons")
+const countPvtDown = document.getElementById("secondsPvt");
+
+function numberRandom() {
+  const min = 1;
+  const max = 100;
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNumber;
+}
+stopSign.addEventListener('mousedown',()=>{
+  let txtPoints;
+  txtPoints = points;
+  document.getElementById("h4").innerHTML = txtPoints;
+  points--;
+})
+clickButtons.addEventListener('mousedown', ()=>{
+  numberRandom();
+  if (numberRandom() > 20 && numberRandom() < 100) {
   greenButton.classList.toggle("pvtN");
   buttonPressed.classList.toggle("pvtN");
   setTimeout(()=>{
     greenButton.classList.toggle("pvtN");
     buttonPressed.classList.toggle("pvtN");
   },100);
+} else {
+  setTimeout(()=>{
+    greenButtons.classList.toggle("pvtN");
+    stopSign.classList.toggle("pvtN");
+  },500);
+  greenButtons.classList.toggle("pvtN");
+  stopSign.classList.toggle("pvtN");
+}
 })
 setTimeout(() => {
   const dis = document.querySelector(".principal");
@@ -66,6 +91,7 @@ setTimeout(() => {
     // win interface
     const rectA = object.getBoundingClientRect();
     const rectW = winner.getBoundingClientRect();
+    const pvtGame = document.querySelector(".pvt-game");
     if (
       rectA.left < rectW.right &&
       rectA.right > rectW.left &&
@@ -90,21 +116,38 @@ setTimeout(() => {
           countEnd.classList.toggle("countDis");
         }, 400)
 
-        // setTimeout(()=>{
-        //   const princPvt = document.querySelector(".principal-pvt")
-        //   screenEnd.classList.add("displayN");
-        //   princPvt.classList.remove("pvtN");
-        //   let tempPvt = setInterval(countFirstPvt, 1000);
-        //   function countFirstPvt(){
-        //     let txtCountPvt;
-        //     txtCountPvt = countPvt;
-        //     document.getElementById("count-pvt").innerHTML = txtCountPvt;
-        //     countPvt--;
-        //     if (countPvt < 0) {
-        //       clearInterval(tempPvt);
-        //     }
-        //   }
-        // },4000);
+     setTimeout(()=>{
+       const princPvt = document.querySelector(".principal-pvt")
+        screenEnd.classList.add("displayN");
+        princPvt.classList.remove("pvtN");
+        let tempPvt = setInterval(countFirstPvt, 1000);
+        function countFirstPvt(){
+          let txtCountPvt;
+          txtCountPvt = countPvt;
+          document.getElementById("count-pvt").innerHTML = txtCountPvt;
+          countPvt--;
+          if (countPvt < -1) {
+            clearInterval(tempPvt);
+            princPvt.classList.add("pvtN");
+            pvtGame.classList.remove("pvtN");
+            let countInterPvt = setInterval(countPvtGame, 1000);
+            function countPvtGame() {
+              let txtCountDownPvt;
+              if (countDownPvt < 10) {
+              txtCountDownPvt = `0${countDownPvt}`;
+              } else {
+                txtCountDownPvt = countDownPvt;
+              }
+              document.getElementById("secondsPvt").innerHTML = txtCountDownPvt;
+              countDownPvt--;
+                
+              if (countDownPvt < 0) {
+                clearInterval(countInterPvt);
+              }
+            }
+          }
+         }
+      },4000);
       },1000);
     } else {
       // No hay colisión
